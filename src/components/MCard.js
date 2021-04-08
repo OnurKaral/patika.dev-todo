8251df2e6b06687607d17e9f54e1c4db1b0ccbd9
OnurKaral <santobixua@gmail.com>
2021-04-08 14:16:47 +0300

    Todo in Card

--- src/components/MCard.js
+++ src/components/MCard.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { Checkbox, Form,Card } from "semantic-ui-react";
 import Button from "@material-ui/core/Button";
+import Todo from './Todo'
+
 
 
 export default class MCard extends Component {
@@ -25,42 +27,10 @@ export default class MCard extends Component {
 
   render() {
     return (
-      <div>
-        <div class="ui card">
-  <div class="content">
-    <div class="header">Project Timeline</div>
-  </div>
-  <div class="content">
-    <h4 class="ui sub header">Activity</h4>
-    <div class="ui small feed">
-      <div class="event">
-        <div class="content">
-          <div class="summary">
-             <a>Elliot Fu</a> added <a>Jenny Hess</a> to the project
-          </div>
-        </div>
-      </div>
-      <div class="event">
-        <div class="content">
-          <div class="summary">
-             <a>Stevie Feliciano</a> was added as an <a>Administrator</a>
-          </div>
-        </div>
-      </div>
-      <div class="event">
-        <div class="content">
-          <div class="summary">
-             <a>Helen Troy</a> added two pictures
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-  <div class="extra content">
-    <button  onClick={this.handel}  class="ui button">Join Project</button>
-  </div>
-</div>
-      </div>
+      <Card>
+      
+          <Todo/>
+    </Card>
     );
   }
 }
